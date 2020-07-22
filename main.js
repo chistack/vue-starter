5725a8f32a8749445ab2ecbb8ee911b5864b4952
Chisom Okpara <chisom@Chisoms-MacBook-Pro.local>
2020-07-22 11:53:14 +0100

    Computed properties

--- main.js
+++ main.js
@@ -1,32 +1,58 @@
 var app = new Vue({
     el: "#app",
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
-        image: './IMG/Socks.jpg',
-        inStock: false,
+        selectedVariant: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: './IMG/Socks.jpg'
+                variantImage: './IMG/Socks.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './IMG/Socks-blue.jpg'
+                variantImage: './IMG/Socks-blue.jpg',
+                variantQuantity: 0
             }
         ],
-        cart: 0
+        cart: 0,
+        onSale: true
     },
 
     methods: {
         addToCart: function() {
             this.cart += 1
         },
-        updateProduct: function(variantImage) {
-            this.image = variantImage
-        },
+        updateProduct: function(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
 
-    }
+    },
+
+    computed: {
+        title () {
+            return this.brand + ' ' + this.product
+        },
+        image () {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock () {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale () {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' ' + 'are on sale!'
+            }
+            return this.brand + ' ' + this.product + ' ' + 'are not on sale!'
+            
+        }
+
+    
+  
+    },
 })
